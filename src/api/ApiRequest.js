c157508d38d46aa48436083aec67c9dda2938574
xuantruong2004 <truongxuantp3@gmail.com>
2022-11-26 17:41:47 +0700

    update API render server

--- src/api/ApiRequest.js
+++ src/api/ApiRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vincent-shop-truongxuan.herokuapp.com",
+  // baseURL: "https://vincent-shop-truongxuan.herokuapp.com",
+  baseURL: "https://vincentmart-server.onrender.com",
 });
 export default API;
